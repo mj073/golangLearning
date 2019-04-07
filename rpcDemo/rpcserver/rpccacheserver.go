3279c3e59cce3a2b14c4b1f1d09cb71b1a39f1e6
Mahesh Jadhav <jadhav.mahesh073@gmail.com>
2019-04-07 18:33:34 +0530

    little modification in rpccache

--- rpcDemo/rpcserver/rpccacheserver.go
+++ rpcDemo/rpcserver/rpccacheserver.go
@@ -1,29 +1,37 @@
 package main
 
 import (
-	"rpcDemo/rpcobject"
-	"net/rpc"
-	"net"
 	"fmt"
-	"os"
+	"github.com/golangLearning/rpcDemo/rpcobject"
 	"log"
+	"net"
 	"net/http"
 	_ "net/http/pprof"
+	"net/rpc"
+	"os"
 )
 
-func main(){
+func main() {
 	// for pprof
 	go func() {
-		log.Println(http.ListenAndServe("192.168.56.100:5555", nil))
+		log.Println(http.ListenAndServe("localhost:5555", nil))
 	}()
 	c := rpcobject.NewRPCCache()
 	rpc.Register(c)
 
-	l, err := net.Listen("tcp","192.168.56.100:2000")
-	if err != nil{
-		fmt.Println("failed to listen on localhost:2000..>ERROR:",err)
+	l, err := net.Listen("tcp", "192.168.1.105:2000")
+	if err != nil {
+		fmt.Println("failed to listen on localhost:2000..>ERROR:", err)
 		os.Exit(1)
 	}
+	/*	for {
+		co, err := l.Accept()
+		if err != nil {
+			fmt.Println("ERROR:",err)
+			continue
+		}
+		rpc.ServeConn(co)
+	}*/
 	rpc.Accept(l)
 
 }
